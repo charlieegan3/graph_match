09f46c9ab049ec4b3a296f6eb09c773e239ca220
Charlie Egan <git@charlieegan3.com>
2016-05-12 23:11:27 +0100

    Add identifier and hashmap attrs to nodes + edges
    This will make nodes in the graph named and capable of storing
    information about different elements of the graph. These can be
    referenced in analysis once a graph has been built up.

--- src/graph.rs
+++ src/graph.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 // http://smallcultfollowing.com/babysteps/blog/2015/04/06/modeling-graphs-in-rust-using-vector-indices/
 
 pub struct Graph {
@@ -6,18 +7,21 @@ pub struct Graph {
 }
 
 impl Graph {
-    pub fn add_node(&mut self) -> NodeIndex {
+    pub fn add_node(&mut self, identifier: String, attributes: Option<HashMap<String,String>>)
+        -> NodeIndex {
         let index = self.nodes.len();
-        self.nodes.push(NodeData { first_outgoing_edge: None });
+        self.nodes.push(NodeData { identifier: identifier, first_outgoing_edge: None, attributes: attributes });
         index
     }
 
-    pub fn add_edge(&mut self, source: NodeIndex, target: NodeIndex) {
+    pub fn add_edge(&mut self, source: NodeIndex, target: NodeIndex, identifier: String, attributes: Option<HashMap<String,String>>) {
         let edge_index = self.edges.len();
         let node_data = &mut self.nodes[source];
         self.edges.push(EdgeData {
+            identifier: identifier,
             target: target,
-            next_outgoing_edge: node_data.first_outgoing_edge
+            next_outgoing_edge: node_data.first_outgoing_edge,
+            attributes: attributes,
         });
         node_data.first_outgoing_edge = Some(edge_index);
     }
@@ -43,13 +47,17 @@ impl Graph {
 
 pub type NodeIndex = usize;
 pub struct NodeData {
+    identifier: String,
+    attributes: Option<HashMap<String, String>>,
     first_outgoing_edge: Option<EdgeIndex>,
 }
 
 pub type EdgeIndex = usize;
 pub struct EdgeData {
+    identifier: String,
     target: NodeIndex,
-    next_outgoing_edge: Option<EdgeIndex>
+    attributes: Option<HashMap<String, String>>,
+    next_outgoing_edge: Option<EdgeIndex>,
 }
 
 pub struct Successors<'graph> {
@@ -74,13 +82,14 @@ impl<'graph> Iterator for Successors<'graph> {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use super::*;
     #[test]
     fn create_small_graph() {
         let mut graph = Graph { nodes: vec![], edges: vec![] };
-        let node0 = graph.add_node();
-        let node1 = graph.add_node();
-        graph.add_edge(node0, node1);
+        let node0 = graph.add_node("node0".to_string(), None);
+        let node1 = graph.add_node("node1".to_string(), None);
+        graph.add_edge(node0, node1, "edge0".to_string(), None);
 
         assert_eq!(graph.nodes.len(), 2);
         assert_eq!(graph.edges.len(), 1);
@@ -93,13 +102,13 @@ mod tests {
     #[test]
     fn create_list_graph() {
         let mut graph = Graph { nodes: vec![], edges: vec![] };
-        let node0 = graph.add_node();
-        let node1 = graph.add_node();
-        let node2 = graph.add_node();
-        let node3 = graph.add_node();
-        graph.add_edge(node0, node1);
-        graph.add_edge(node1, node2);
-        graph.add_edge(node2, node3);
+        let node0 = graph.add_node("node0".to_string(), None);
+        let node1 = graph.add_node("node1".to_string(), None);
+        let node2 = graph.add_node("node2".to_string(), None);
+        let node3 = graph.add_node("node3".to_string(), None);
+        graph.add_edge(node0, node1, "edge0".to_string(), None);
+        graph.add_edge(node1, node2, "edge1".to_string(), None);
+        graph.add_edge(node2, node3, "edge2".to_string(), None);
 
         let mut targets = vec![];
         for e in graph.edges {
@@ -111,10 +120,10 @@ mod tests {
     #[test]
     fn list_node_edges() {
         let mut graph = Graph { nodes: vec![], edges: vec![] };
-        let node0 = graph.add_node();
-        graph.add_edge(node0, node0);
-        graph.add_edge(node0, node0);
-        graph.add_edge(node0, node0);
+        let node0 = graph.add_node("node0".to_string(), None);
+        graph.add_edge(node0, node0, "edge0".to_string(), None);
+        graph.add_edge(node0, node0, "edge1".to_string(), None);
+        graph.add_edge(node0, node0, "edge2".to_string(), None);
         let mut targets = vec![];
         let mut successors = graph.successors(0);
         loop {
@@ -124,5 +133,44 @@ mod tests {
             }
         }
         assert_eq!(targets, [0,0,0]);
+    }
+
+    #[test]
+    fn node_attributes() {
+        let mut graph = Graph { nodes: vec![], edges: vec![] };
+
+        let mut attributes = HashMap::new();
+        attributes.insert("key".to_string(), "value".to_string());
+
+        let node0 = graph.add_node("node0".to_string(), Some(attributes));
+        match graph.nodes[node0].attributes {
+            Some(ref attrs) => {
+                match attrs.get("key") {
+                    Some(value) => assert_eq!(value, &"value".to_string()),
+                    None => assert!(false),
+                }
+            },
+            None => assert!(false),
+        }
+    }
+
+    #[test]
+    fn edge_attributes() {
+        let mut graph = Graph { nodes: vec![], edges: vec![] };
+
+        let mut attributes = HashMap::new();
+        attributes.insert("key".to_string(), "value".to_string());
+
+        let node0 = graph.add_node("node0".to_string(), None);
+        graph.add_edge(node0, node0, "edge0".to_string(), Some(attributes));
+        match graph.edges[0].attributes {
+            Some(ref attrs) => {
+                match attrs.get("key") {
+                    Some(value) => assert_eq!(value, &"value".to_string()),
+                    None => assert!(false),
+                }
+            },
+            None => assert!(false),
+        }
     }
 }
