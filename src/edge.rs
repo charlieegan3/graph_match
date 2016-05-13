e6a8f2ce3a6ce3f46b59422f5c942a21d90bd56e
Charlie Egan <git@charlieegan3.com>
2016-05-13 22:40:18 +0100

    Add basic total equality matcher for Edge
    Fuzzy matcher to come later, which will be more useful.

--- src/edge.rs
+++ src/edge.rs
@@ -9,3 +9,44 @@ pub struct Edge {
     pub attributes: Option<HashMap<String, String>>,
     pub next_outgoing_edge: Option<Index>,
 }
+
+impl Edge {
+    pub fn equal(&self, edge: Edge) -> bool {
+        self.identifier == edge.identifier &&
+            self.attributes == edge.attributes
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::*;
+    #[test]
+    fn edge_equality() {
+        let mut attributes = HashMap::new();
+        attributes.insert("key".to_string(), "value".to_string());
+        let mut attributes2 = HashMap::new();
+        attributes.insert("core".to_string(), "worth".to_string());
+
+        let edge0 = Edge {
+            identifier: "edgeid".to_string(),
+            attributes: Some(attributes.clone()),
+            next_outgoing_edge: None,
+            target: 0,
+        };
+        let edge1 = Edge {
+            identifier: "edgeid".to_string(),
+            attributes: Some(attributes.clone()),
+            next_outgoing_edge: None,
+            target: 0,
+        };
+        let edge2 = Edge {
+            identifier: "edgeid".to_string(),
+            attributes: Some(attributes2.clone()),
+            next_outgoing_edge: None,
+            target: 0,
+        };
+        assert!(edge0.equal(edge1));
+        assert_eq!(edge0.equal(edge2), false);
+    }
+}
