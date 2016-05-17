ec35ec5396249b2c57fc3e61e42a88d91d795588
Charlie Egan <git@charlieegan3.com>
2016-05-17 21:56:13 +0100

    Implement basic traversal restriction
    A vector of banned identifiers can now be passed to filter relations
    permitted in the traversal. In time this would become a list of sample
    edges that could be fuzzy matched against.

--- src/lib.rs
+++ src/lib.rs
@@ -19,16 +19,22 @@ pub fn match_graph(query: graph::Graph, query_root_index: node::Index, graph: gr
     return matching::recusive_node_match(query_root_index, graph_root_index.unwrap(), &query, &graph);
 }
 
-pub fn expand_subgraph(graph: &graph::Graph, root_index: node::Index) -> Vec<node::Index> {
-    let mut node_list = vec![root_index];
-    let connected_nodes: Vec<node::Index> = graph.edges_for_node(root_index).iter().map(|&e| graph.edges[e].target).collect();
-    for node in connected_nodes {
-        for inner_node in expand_subgraph(graph, node) {
-            node_list.push(inner_node);
+pub fn expand_subgraph(
+    graph: &graph::Graph, root_index: node::Index, banned_identifiers: &Vec<String>)
+    -> Vec<node::Index> {
+        let mut node_list = vec![root_index];
+        let root_node_edges: Vec<node::Index> = graph.edges_for_node(root_index);
+        for edge in root_node_edges {
+            if banned_identifiers.contains(&graph.edges[edge].identifier) {
+                continue;
+            }
+            let target = graph.edges[edge].target;
+            for inner_node in expand_subgraph(graph, target, &banned_identifiers) {
+                node_list.push(inner_node);
+            }
         }
-    }
 
-    return node_list;
+        return node_list;
 }
 
 #[cfg(test)]
@@ -38,7 +44,7 @@ mod tests {
     use graph;
 
     #[test]
-    fn complete_traverse() {
+    fn traversal_simple() {
         let mut simple_graph = graph::Graph { nodes: vec![], edges: vec![] };
         let node0 = simple_graph.add_node("node0".to_string(), None);
         let node1 = simple_graph.add_node("node1".to_string(), None);
@@ -46,11 +52,11 @@ mod tests {
         simple_graph.add_edge(node0, node1, "edge0".to_string(), None);
         simple_graph.add_edge(node1, node2, "edge1".to_string(), None);
 
-        assert_eq!(expand_subgraph(&simple_graph, 0), vec![0,1,2]);
+        assert_eq!(expand_subgraph(&simple_graph, 0, &vec![]), vec![0,1,2]);
     }
 
     #[test]
-    fn incomplete_traverse() {
+    fn traveral_incomplete() {
         let mut simple_graph = graph::Graph { nodes: vec![], edges: vec![] };
         let node0 = simple_graph.add_node("node0".to_string(), None);
         let node1 = simple_graph.add_node("node1".to_string(), None);
@@ -58,7 +64,21 @@ mod tests {
         simple_graph.add_edge(node0, node1, "edge0".to_string(), None);
         simple_graph.add_edge(node2, node1, "edge1".to_string(), None);
 
-        assert_eq!(expand_subgraph(&simple_graph, 0), vec![0,1]);
+        assert_eq!(expand_subgraph(&simple_graph, 0, &vec![]), vec![0,1]);
+    }
+
+    #[test]
+    fn traveral_restricted() {
+        let mut simple_graph = graph::Graph { nodes: vec![], edges: vec![] };
+        let node0 = simple_graph.add_node("node0".to_string(), None);
+        let node1 = simple_graph.add_node("node1".to_string(), None);
+        let node2 = simple_graph.add_node("node2".to_string(), None);
+        let node3 = simple_graph.add_node("node3".to_string(), None);
+        simple_graph.add_edge(node0, node1, "edge0".to_string(), None);
+        simple_graph.add_edge(node1, node2, "banned".to_string(), None);
+        simple_graph.add_edge(node1, node3, "edge2".to_string(), None);
+
+        assert_eq!(expand_subgraph(&simple_graph, 0, &vec!["banned".to_string()]), vec![0,1,3]);
     }
 
     #[test]
@@ -102,7 +122,7 @@ mod tests {
     }
 
     #[test]
-    fn fail_to_match() {
+    fn match_failure() {
         let mut attributes = HashMap::new();
         attributes.insert("key".to_string(), "value".to_string());
         let mut simple_graph = graph::Graph { nodes: vec![], edges: vec![] };
